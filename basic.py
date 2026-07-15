37191014f4078eb149e00e2b6e7f82dbf2a1f56b
dotzohaib <dayozohaibali@gmail.com>
2026-07-15 07:35:09 +0500

    task

--- basic.py
+++ basic.py
@@ -10,6 +10,24 @@
 
 value = ["Coding", "Testing", "Deployment", "Coding", "Debugging"]
 
+value.remove("Coding")
+print(value)
+
 
 # print(value[2:1])
-print(value[2:1:-1])
+# print(value[2:1:-1])
+
+
+# for i in value:
+#     print(i)
+
+# for key,value in enumerate(value):
+#     print(key, value)
+
+
+# if "Deploymentw" in value:
+#     print("Yes, Deployment is present in the list")
+# else:
+#     print("No, Deployment is not present in the list")
+
+
