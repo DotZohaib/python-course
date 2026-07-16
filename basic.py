cf9265e96a1fbdef706eca4843ff0f5bdc2a0fb4
dotzohaib <dayozohaibali@gmail.com>
2026-07-16 06:18:31 +0500

    tuple

--- basic.py
+++ basic.py
@@ -8,10 +8,10 @@
 
 # print(data)
 
-value = ["Coding", "Testing", "Deployment", "Coding", "Debugging"]
+# value = ["Coding", "Testing", "Deployment", "Coding", "Debugging"]
 
-value.remove("Coding")
-print(value)
+# value.remove("Coding")
+# print(value)
 
 
 # print(value[2:1])
@@ -31,3 +31,19 @@ print(value)
 #     print("No, Deployment is not present in the list")
 
 
+
+my_tuple = ("apple", "banana", "cherry")
+
+# my_tuple[1] = "kiwi"  # This will raise an error because tuples are immutable
+data = my_tuple.count("apple")  # This will return the count of "apple" in the tuple
+ind = my_tuple.index("banana")  # This will return the index of "banana" in the tuple
+
+new_tuple = my_tuple + ("orange",)  # This will create a new tuple by concatenating another tuple
+extra_tuple = ("grape", "kiwi")
+new_tuple = new_tuple + extra_tuple  # This will create a new tuple by concatenating another tuple
+
+
+print(my_tuple)
+print(data)
+print(ind)
+print(new_tuple)
